4baf0d56fd3558a1207cfa443a01df1195334bcd
Evgenii Borisovich <anarkilimitz@gmail.com>
2025-08-01 13:00:09 +0500

    final

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const myParallax = new Parallax(document.querySelectorAll('.parallax'));
+
+window.addEventListener('scroll', function () {
+	const footer = document.querySelector('.footer-img');
+	const triggerPoint = 250;
+
+	if (window.scrollY > triggerPoint) {
+		footer.classList.add('visible');
+	} else {
+		footer.classList.remove('visible');
+	}
+});
